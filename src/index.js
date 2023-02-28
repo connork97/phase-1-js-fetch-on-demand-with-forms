4a8dc9c3a2a026e2657e13cbc33151dd3c34a872
Connor Kormos <connorkormos97@gmail.com>
2023-02-28 10:37:32 -0800

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const userInput = document.querySelector("input#searchByID");
+    console.log(userInput.value);
+
+    fetch(`http://localhost:3000/movies/${userInput.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+    .catch(err => console.log(err))
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
